85c7886cd95b4c2fd73c70a57bb6006586f41438
TheFrenchTechMan <95764063+TheFrenchTechMan@users.noreply.github.com>
2025-10-24 23:34:44 +0200

    a

---
+++ main.py
@@ -0,0 +1,18 @@
+RUNES = [
+    {
+        "name": "Precision",
+        "major_runes": [
+            1, 2, 3, 4
+        ],
+        "minor_runes": [
+            [
+                78, #Absorb Life
+                17, #Triumph
+                18  #Presence of Mind
+            ],
+            [
+                
+            ]
+        ]
+    }
+]
